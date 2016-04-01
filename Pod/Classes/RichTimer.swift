b74a8a5501cb2f4991d575f0b54dd6c546636c94
Pilipenko Dima <dimpiax@gmail.com>
2016-04-01 19:25:24 +0300

    Initial commit

--- Pod/Classes/RichTimer.swift
+++ Pod/Classes/RichTimer.swift
@@ -9,15 +9,71 @@
 import Foundation
 
 public extension NSTimer {
-    public class func scheduledTimerWithTimeInterval(ti: NSTimeInterval, target aTarget: AnyObject, selector aSelector: Selector) -> NSTimer {
-        return scheduledTimerWithTimeInterval(ti, target: aTarget, selector: aSelector, userInfo: nil, repeats: false)
+    class func once(value: NSTimeInterval, completion: () -> Void) -> NSTimer {
+        return initTimer(value, userInfo: VoidBox(nilCompletion: completion), repeats: false)
     }
     
-    class func delay(ti value: NSTimeInterval) -> NSTimer {
-        return scheduledTimerWithTimeInterval(value, target: self, selector: #selector(a(_:)))
+    class func each(value: NSTimeInterval, completion: () -> Void) -> NSTimer {
+        return initTimer(value, userInfo: VoidBox(nilCompletion: completion), repeats: true)
     }
     
-    class func a(timer: NSTimer) {
-        print("whoop!")
+    class func once<T>(value: NSTimeInterval, arguments: T, completion: T -> Void) -> NSTimer {
+        return initTimer(value, userInfo: Box(arguments, completion: completion), repeats: false)
     }
+    
+    class func each<T>(value: NSTimeInterval, arguments: T, completion: T -> Void) -> NSTimer {
+        return initTimer(value, userInfo: Box(arguments, completion: completion), repeats: true)
+    }
+    
+    class func timerDidFired(timer: NSTimer) {
+        let box = timer.userInfo! as! Boxable
+        box.lookInside()
+    }
+    
+    private class func initTimer(ti: NSTimeInterval, userInfo: AnyObject, repeats: Bool = false) -> NSTimer {
+        return scheduledTimerWithTimeInterval(ti, target: self, selector: #selector(timerDidFired(_:)), userInfo: userInfo, repeats: repeats)
+    }
+}
+
+private class VoidBox: Boxable {
+    typealias CompletionType = () -> Void
+    
+    private var _completion: CompletionType!
+    
+    init(nilCompletion completion: CompletionType) {
+        self._completion = completion
+    }
+    
+    func lookInside() {
+        _completion()
+    }
+    
+    deinit {
+        _completion = nil
+    }
+}
+
+private class Box<T>: Boxable {
+    typealias CompletionType = T -> Void
+    
+    private var _arguments: T!
+    private var _completion: CompletionType!
+    
+    init(_ arguments: T!, completion: CompletionType) {
+        self._arguments = arguments
+        self._completion = completion
+    }
+    
+    func lookInside() {
+        _completion(_arguments)
+    }
+    
+    deinit {
+        _arguments = nil
+        _completion = nil
+    }
+}
+
+private protocol Boxable {
+    func lookInside()
 }
